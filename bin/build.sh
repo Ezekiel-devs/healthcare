75248f279cadf823a136edf42f09a57bc481acc3
Ezekiel-devs <tchatchuezekiel@gmail.com>
2025-06-15 06:36:20 +0100

    Automate superuser creation in build script

--- bin/build.sh
+++ bin/build.sh
@@ -6,3 +6,9 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+echo "Creating superuser..."
+# Cette commande utilise les variables d'environnement pour créer l'utilisateur
+# Le || true à la fin est une sécurité : si l'utilisateur existe déjà, la commande échouera
+# mais le "|| true" empêchera le build de planter.
+python manage.py createsuperuser --noinput || true
